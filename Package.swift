98fbf79230b81fc30a25c7580d01307f47813b00
Arslan Ablikim <arslan.ablikim0@gmail.com>
2016-12-27 15:45:15 +0800

    kitura BareBones

--- Package.swift
+++ Package.swift
@@ -5,5 +5,15 @@
 import PackageDescription
 
 let package = Package(
-        name: "Sparkle Spec Collector"
+        name: "Sparkle Spec Collector",
+        targets: [
+                Target(name: "Sparkle Spec Collector", dependencies: [])
+        ],
+        dependencies: [
+                .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
+                .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 3),
+                .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 9),
+                .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 8)
+        ],
+        exclude: ["Makefile", "Package-Builder"]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -3,5 +3,26 @@
 //
 
 import Foundation
+import HeliumLogger
+import Foundation
+import Kitura
+import LoggerAPI
+import SwiftyJSON
 
-print("Hello world")
+// Disable buffering
+setbuf(stdout, nil)
+
+// Attach a logger
+Log.logger = HeliumLogger()
+
+// setup routes
+let router = Router()
+router.get("/") { _, response, next in
+    response.status(.OK).send(json: JSON(["hello" : "world"]))
+    next()
+}
+
+// Start server
+Log.info("Starting server")
+Kitura.addHTTPServer(onPort: 8090, with: router)
+Kitura.run()
