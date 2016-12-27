e77abf478f087e9aa08408f20cfbbae6d348c4b3
Arslan Ablikim <arslan.ablikim0@gmail.com>
2016-12-27 15:07:35 +0800

    Hello World BareBone

---
+++ Package.swift
@@ -0,0 +1,9 @@
+//
+// Created by arslan on 12/27/16.
+//
+
+import PackageDescription
+
+let package = Package(
+        name: "Sparkle Spec Collector"
+)
---
+++ Sources/main.swift
@@ -0,0 +1,7 @@
+//
+// Created by arslan on 12/27/16.
+//
+
+import Foundation
+
+print("Hello world")
