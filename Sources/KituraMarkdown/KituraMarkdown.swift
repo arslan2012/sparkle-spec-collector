4eab6bf8399bad5d6b7384e0ccd40f9ac67197ea
Arslan Ablikim <arslan.ablikim0@gmail.com>
2017-01-03 21:46:44 +0800

    add char set so that it displays chinese

--- Sources/KituraMarkdown/KituraMarkdown.swift
+++ Sources/KituraMarkdown/KituraMarkdown.swift
@@ -71,6 +71,8 @@ public class KituraMarkdown: TemplateEngine {
     public static func render(from: Data) -> String {
         return from.withUnsafeBytes() { (bytes: UnsafePointer<Int8>) -> String in
 			var html = "<!DOCTYPE html>\n<html>"
+			
+			html += "<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /></head>"
         
             guard let htmlBytes = cmark_markdown_to_html(bytes, from.count, 0) else { return "" }
 
